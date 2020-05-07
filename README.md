6afa606e82b4bebf3b011df2bffa4e20e05d7a79
Tim Wiel <timwiel@gmail.com>
2020-05-07 20:06:52 +1200

    README Updates

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # libreoffice2git
-Bashscript to convert Libreoffice Documents to Git Friendly Format
+A bashscript to convert Libreoffice Writer Documents (.odt) to a Git Friendly Format with text based diff available
+
+## Preamble
+
+Libreoffice file formats are ZIP archives with XML files in them containing the content and styles. As ZIP archives are binary files unfortunately Git (and therefore GitHub, GitLab,etc.) won't display a nice diff for them. 
+
+This script provides a work-around in the meantime by creating a folder for each .ODT file and placing within it content that can easily have diff performed on it with good results.
+
+## Features
+
+- Creates a subfolder called <FILENAME.odt.git> in the folder the .odt file resides in
+- Creates Markdown and Plaintext using pandoc
+- Optionally:
+  - Exports embeded pictures from the .odt file
+  - Exports the whole "flat odt xml" file but tidies the XML using xmllint for easier comparisions
+
+## Usage
+
+```
+USAGE: ./libreoffice2git.sh [-hpz] -i <file>
+
+  where:
+    -i  --input         Input file (.odt format)
+    -p  --pictures      Extract the pictures
+    -z  --unzip         Save the full unzipped ODT (includes -p)
+    -h  --help          show this help text
+```
+
+Please note that this script requires unzip, pandoc and xmllint to be installed. These are easily installed on Mac (from Homebrew)(e.g. `brew install pandoc`) or Ubuntu (e.g. `apt-get install pandoc`)
+
+## Examples
+
+Extract just markdown and plaintext from resume.odt
+
+```
+./libreoffice2git.sh --input resume.odt
+```
+
+Extract just markdown and plaintext with pictures from resume.odt
+
+```
+./libreoffice2git.sh --pictures --input resume.odt
+```
+
+Flatten the .odt into xml, pictures with markdown and plaintext from resume.odt
+
+```
+./libreoffice2git.sh --pictures --unzip --input resume.odt
+```
+
+## Based on
+
+This script is based on the powershell script for Microsoft Word .docx files by Tomáš Hübelbauer found at https://github.com/TomasHubelbauer/modern-office-git-diff
---
+++ libreoffice2git.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+# TITLE
+# Libreoffice ODT to GIT-Friendly
+
+# DESCRIPTION
+# Script for converting Libreoffice ODT file format to a git safe format without modifying original document
+
+# SEE ALSO
+# https://github.com/TomasHubelbauer/modern-office-git-diff
+
+# Author          Tim Wiel <timwiel@gmail.com>
+# Version         20200507
+
+
+
+#Variables
+ARG_PICTURES=0
+ARG_UNZIP=0
+PROG_NAME=$0
+
+#Check required commands are installed
+if ! [ -x "$(command -v unzip)" ]; then
+  echo 'Error: unzip is not installed.' >&2
+  exit 1
+fi
+if ! [ -x "$(command -v pandoc)" ]; then
+  echo 'Error: pandoc is not installed.' >&2
+  exit 1
+fi
+if ! [ -x "$(command -v xmllint)" ]; then
+  echo 'Error: xmllint is not installed.' >&2
+  exit 1
+fi
+
+# Transform long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    "--input")      set -- "$@" "-i" ;;
+    "--pictures")   set -- "$@" "-p" ;;
+    "--unzip")      set -- "$@" "-z" ;;
+    "--help")       set -- "$@" "-h" ;;
+    *)              set -- "$@" "$arg"
+  esac
+done
+
+#Specify Usage
+function help {
+  echo "USAGE: $PROG_NAME [-hpz] -i <file>"
+  echo ""
+  echo "  where:
+    -i  --input         Input file (.odt format)
+    -p  --pictures      Extract the pictures
+    -z  --unzip         Save the full unzipped ODT (includes -p)
+    -h  --help          show this help text
+  "
+  exit 1
+}
+
+#Check for help and invalid options
+while getopts ":hi:pz" opt; do
+  case $opt in
+    h) # Display help
+        help
+        exit;;
+    i) # Check file input is correct
+        ARG_FILENAME="$OPTARG"
+        if [[ $(file --mime-type -b "$ARG_FILENAME") != application/vnd.oasis.opendocument.text ]]; then
+          echo "ERROR: Invalid filetype"
+          echo ""
+          help
+        fi
+        ;;
+    p) # Save the unzipped ODT contents
+        ARG_PICTURES=1
+        ;;
+    z) # Save the unzipped ODT contents
+        ARG_UNZIP=1
+        ;;
+    \?) # incorrect option
+      echo "ERROR: Invalid option"
+      echo ""
+      help
+      exit;;
+  esac
+done
+
+#Change to the directory of the file
+cd $(dirname "$ARG_FILENAME")
+FILE_BASENAME=$(basename "$ARG_FILENAME")
+GIT_DIR="$FILE_BASENAME.git"
+
+#Make the directory if it doesn't exist
+echo "Progress:  Creating Git Directory for ......... $FILE_BASENAME"
+mkdir -p $GIT_DIR
+
+#Unzip the odt file and change to that directory
+if [ $ARG_PICTURES == 1 ]; then
+  echo "Progress:  Extractig Pictures ................. $FILE_BASENAME"
+  unzip -qo $FILE_BASENAME 'Pictures/*' -d $GIT_DIR
+fi
+
+#Unzip the odt file and change to that directory
+if [ $ARG_UNZIP == 1 ]; then
+  echo "Progress:  Unzipping .......................... $FILE_BASENAME"
+  unzip -qo $FILE_BASENAME -d $GIT_DIR
+fi
+
+#Change to GIT DIR
+cd $GIT_DIR
+
+#Create raw text formats
+echo "Progress:  Creating markdown file of .......... $FILE_BASENAME"
+pandoc -f odt -i ../$FILE_BASENAME -s -t markdown_strict -o content.md
+
+echo "Progress:  Creating text file of .............. $FILE_BASENAME"
+pandoc -f odt -i ../$FILE_BASENAME -s -t plain -o content.txt
+
+if [ $ARG_UNZIP == 1 ]; then
+  echo "Progress:  Linting xml from ................... $FILE_BASENAME"
+  for f in *.xml; do
+    echo $f
+    xmllint --format $f --output $f
+  done
+fi
+
+#Exit the script
+exit 0
